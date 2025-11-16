1dde4d4fed5dbaf368d350e0c318d9d8a44836b1
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2025-11-16 17:28:46 -0300

    Update page.js

--- src/app/pagError/page.js
+++ src/app/pagError/page.js
@@ -15,8 +15,19 @@ const poppins = Poppins({
 export default function pagError() {
   return (
     <div className="w-screen min-h-screen flex justify-center items-center bg-error-gradient">
-      <div className="bg-[#f2f2f2] w-[90vw] min-h-[80vh] rounded-4xl flex text-black animate-slideUp">
-        
+      <div className="bg-[#f2f2f2] w-[90vw] min-h-[80vh] rounded-4xl flex text-black animate-slideUp ">
+        <div className="flex bg-gray-300 w-[100px] h-[50px] rounded-4xl m-4 fixed
+    justify-center items-center 
+    hover:bg-gray-400 hover:cursor-pointer hover:text-[#f2f2f2]
+    transition-all duration-200 delay-100 hover:scale-105 ease-in-out">
+
+                    <a className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
+                        </svg>
+                        <p className="font-medium ml-1">Voltar</p>
+                    </a>
+                </div>
         <div className="flex flex-col justify-center">
           <h1 className={`${poppins.className} text-[65px] font-bold m-8 mb-6 ml-12`}>
             Algo deu errado 😢
