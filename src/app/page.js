7a049095312abd3775e9286ffe97409f2bea2d5d
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2025-11-22 00:40:52 -0300

    add responsividade pt-2

--- src/app/page.js
+++ src/app/page.js
@@ -18,19 +18,19 @@ export default function Home() {
           src={"wedding cake-bro (2).svg"}
           width={600}
           height={200} 
-          className="absolute left-0 ml-[15vw] z-50"/>
+          className="absolute hidden left-0 ml-[15vw] z-50 lg:block"/>
           <Image
           src={"wedding cake-bro (2).svg"}
           width={600}
           height={200} 
-          className="absolute left-0 ml-[15vw] opacity-10 scale-[102%] blur-xl z-0"/>
-        <div className="bg-[#fee68542] w-[40vw] min-h-screen absolute right-0 backdrop-blur-[10px] border-l-2 border-[#ffffff17] flex justify-center items-center flex-col">
+          className="absolute left-0 hidden ml-[15vw] opacity-10 scale-[102%] blur-xl z-0 lg:block"/>
+        <div className="bg-[#fee68542] w-screen min-h-screen absolute right-0 backdrop-blur-[10px] border-l-2 border-[#ffffff17] flex justify-center items-center flex-col  lg:opacity-100 lg:w-[40vw]">
 
-          <div className="border-[#ffffff93] bg-[#ffffff80] border-2 rounded-[32px] w-[80%] h-[70vh] shadow-[0px_0px_16px_0px_rgba(0,_0,_0,_0.1)] flex flex-col justify-center">
-            <h1 className={`${poppins.className} text-[31px] text-[#141414] font-bold text-center mt-2`}>
+          <div className="border-[#ffffff93] bg-[#ffffff80] border-2 rounded-[32px] w-[95%] h-[80vh] shadow-[0px_0px_16px_0px_rgba(0,_0,_0,_0.1)] flex flex-col justify-center lg:w-[80%] lg:h-[70vh]">
+            <h1 className={`${poppins.className} text-[50px] text-[#141414] font-bold text-center mt-2 lg:text-[31px]`}>
               Olá é muito bom ter você conosco!
             </h1>
-            <p className={`${poppins.className} text-[#141414] font-medium text-center m-6 mt-2 mb-2 leading-8`}>Estamos muito felizes por dividir este capítulo tão especial da nossa história com você! Criamos este espaço com muito carinho para que você possa fazer parte deste momento único. Cada gesto seu, cada mensagem e cada presença tornam esse dia ainda mais especial. Seja bem-vindo(a) ao nosso cantinho de celebração!</p>
+            <p className={`${poppins.className} text-[#141414] font-medium text-center m-2 mt-2 mb-2 leading-10 lg:m-6 lg:leading-8`}>Estamos muito felizes por dividir este capítulo tão especial da nossa história com você! Criamos este espaço com muito carinho para que você possa fazer parte deste momento único. Cada gesto seu, cada mensagem e cada presença tornam esse dia ainda mais especial. Seja bem-vindo(a) ao nosso cantinho de celebração!</p>
             <a href={"/ListaCasamento"} target="_blank"><Button /></a>
             
           </div>
