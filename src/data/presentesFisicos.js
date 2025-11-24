9197ed8b09a789a9d3d67e5d9de771a5763b66a5
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2025-11-23 23:13:00 -0300

    add update

--- src/data/presentesFisicos.js
+++ src/data/presentesFisicos.js
@@ -1,18 +1,172 @@
 export const PRESENTES_FISICOS = [ 
    {
-      nome: "Conjunto de Toalhas",
-      imagem: "/presentes/toalhas.jpg",
-      link: "https://www.exemplo.com/toalhas"
+      nome: "Multiprocessador Philco 1000W 12 em 1 All In One Maximus",
+      imagem: "/multiprocessador.jpeg",
+      link: "https://www.philco.com.br/multiprocessador-philco-all-in-one-maximus-1000w/p?idsku=1369&gad_source=4&gad_campaignid=21865396475&gbraid=0AAAAAC_TFqUeVjMhP-G0kdlOs-3Y_G0mD&gclid=CjwKCAiA8vXIBhAtEiwAf3B-g5DDJjZY7x5IByn7aMwqh9uYpNlRFhHZTsu8ltuM9Jnn8SgAltvamRoCx1EQAvD_BwE"
    },
    {
       nome: "Jogo de Panelas",
       imagem: "/presentes/panelas.jpg",
-      link: "https://www.exemplo.com/panelas"
+      link: "https://www.amazon.com.br/BATEDEIRA-PLANETARIA-PBP600P-TURBO-DOUBLE/dp/B0891DKDPN/ref=asc_df_B0891DKDPN?mcid=8d0bfe3710753a3f9bfb274cda13c430&tag=googleshopp00-20&linkCode=df0&hvadid=709964506241&hvpos=&hvnetw=g&hvrand=11869882910127801381&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-974960183670&psc=1&language=pt_BR&gad_source=1"
    },
    {
       nome: "Cafeteira Elétrica",
       imagem: "/presentes/cafeteira.jpg",
-      link: "https://www.exemplo.com/cafeteira"
+      link: "https://www.amazon.com.br/Cer%C3%A2mica-Antiaderente-Compat%C3%ADvel-Vitrocer%C3%A2mico-Brinovar/dp/B0FQX9F6KY/ref=asc_df_B0FQX9F6KY?mcid=5e07bdd01e903d6aa332853b0bcc248a&tag=googleshopp00-20&linkCode=df0&hvadid=773067503959&hvpos=&hvnetw=g&hvrand=13999084719999141635&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2444134659998&psc=1&language=pt_BR&gad_source=1"
    },
-   
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.magazineluiza.com.br/panela-de-pressao-eletrica-electrolux-por-rita-lobo-6l-preta-experience-digital-pcc20/p/729222300/ep/eppe/?seller_id=electrolux"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Fritadeira-El%C3%A9trica-programada-girat%C3%B3rio-Electrolux/dp/B0BYK9KP3W/ref=asc_df_B0BYK9KP3W?mcid=215d589f99ab3608bc30181f2d9e10a5&tag=googleshopp00-20&linkCode=df0&hvadid=709964506256&hvpos=&hvnetw=g&hvrand=17709458358040957111&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2204411115511&language=pt_BR&gad_source=1&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.magazineluiza.com.br/passadeira-a-vapor-portatil-mondial-vip-care-vp-07-2l-1800w-branco-e-azul/p/236684500/"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Electrolux-antiaderente-ceramica-vertical-ESI60/dp/B0BX6W6V32/ref=asc_df_B0BX6W6V32?mcid=5af97885eb3b3476881a5351c4c5bc91&tag=googleshopp00-20&linkCode=df0&hvadid=709964506301&hvpos=&hvnetw=g&hvrand=15036366579222119529&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2202900609097&language=pt_BR&gad_source=1&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Purificador-Electrolux-Compacto-Cinza-Bivolt/dp/B0DW96BN1W/ref=asc_df_B08X66RQ48?mcid=0ee5c51596ef30da9e47c7a7f7e95b40&tag=googleshopp00-20&linkCode=df0&hvadid=709874962041&hvpos=&hvnetw=g&hvrand=7046093628144729820&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-1188621312704&language=pt_BR&gad_source=1&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Tramontina-64510890-Servir-Pe%C3%A7as-Prata/dp/B076JJYJND/ref=asc_df_B076JJYJND?mcid=4122e00981383ed6930c40b30d37e13f&tag=googleshopp00-20&linkCode=df0&hvadid=709964506304&hvpos=&hvnetw=g&hvrand=13171728295434891245&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-812734424598&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.nescafe-dolcegusto.com.br/maquinas-cafe/genio-s-touch-prata-110v-1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Retr%C3%A1til-Reclin%C3%A1vel-Lugares-Veludo-Coliseu/dp/B0F497SC79/ref=asc_df_B0F497SC79?mcid=b35a7c948b3d38a58e9cd29f3e3ff84c&tag=googleshopp00-20&linkCode=df0&hvadid=724150830217&hvpos=&hvnetw=g&hvrand=9501368524284052869&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2421703891638&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Cama-Box-Queen-Colch%C3%A3o-Molas/dp/B0894KH3NG/ref=asc_df_B0894KH3NG?mcid=2c5b24e84ecc3b75b4821e3868d15912&tag=googleshopp00-20&linkCode=df0&hvadid=709968340954&hvpos=&hvnetw=g&hvrand=12643972285502061028&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2060669609479&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Painel-Prateleiras-Nair%C3%B3bi-Multim%C3%B3veis-Grafite/dp/B09BZB11KQ/ref=asc_df_B09BZB11KQ?mcid=13eee422a9c03887a8131114f251ff78&tag=googleshopp00-20&linkCode=df0&hvadid=709968341269&hvpos=&hvnetw=g&hvrand=18099477653257915357&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2015793248344&language=pt_BR&gad_source=1&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica1",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Condicionado-Split-Inverter-Philco-PAC12FC/dp/B0DVMS874C/ref=asc_df_B0DVMS874C?mcid=9dfe49871e13316e83077400ed2d18cb&tag=googleshopp00-20&linkCode=df0&hvadid=709968341215&hvpos=&hvnetw=g&hvrand=14356097392387763039&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2417964165302&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/ClosetMaid-prateleira-armazenamento-modular-pendurar/dp/B0DQ6CL2M9"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Electrolux-4011AHBR407-Mixer-EIB10/dp/B09B2M8N4T/ref=asc_df_B09B2M8N4T?mcid=25dceb4e72ac342a988f7ad76d8c2994&tag=googleshopp00-20&linkCode=df0&hvadid=709874962041&hvpos=&hvnetw=g&hvrand=16046829737218168317&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-1416833357664&language=pt_BR&gad_source=1&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/GW-01-Waffle-Maker-Mondial-Preto/dp/B0758BTB1M/ref=asc_df_B0758BTB1M?mcid=3436e73ef3ed360ebc76681aa303f24e&tag=googleshopp00-20&linkCode=df0&hvadid=709964506256&hvpos=&hvnetw=g&hvrand=3935734103200374046&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-540765960148&language=pt_BR&gad_source=1&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Liquidificador-Philco-1400w-Limpa-Plq1430p/dp/B09XBM9KG4/ref=asc_df_B09XBM9KG4?mcid=960da54473fe37ae96d2115b9046da47&tag=googleshopp00-20&linkCode=df0&hvadid=709864975911&hvpos=&hvnetw=g&hvrand=1356293465683671001&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-1884372546709&language=pt_BR&gad_source=4&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/APARELHO-JANTAR-ESTALEIRO-PORCELANA-PRATEADA/dp/B0DV78HH28/ref=asc_df_B0DV78HH28?mcid=724610ede3bc30038094d49ef0f53756&tag=googleshopp00-20&linkCode=df0&hvadid=709964506244&hvpos=&hvnetw=g&hvrand=2977072262313381835&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2428378012058&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Aparelho-Jantar-Ch%C3%A1-Oxford-Maresia/dp/B0BZ53C13F/ref=asc_df_B0BZ53C13F?mcid=0200538440583032975f41541f092bb9&tag=googleshopp00-20&linkCode=df0&hvadid=709964506244&hvpos=&hvnetw=g&hvrand=7777434907563886907&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2201406492938&psc=1&language=pt_BR&gad_source=4"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Electrolux-4310AOBR407-Torradeira-ETS25/dp/B09CBWDVH4/ref=asc_df_B09CBWDVH4?mcid=c04f41bc70733e76bb670aa0874af899&tag=googleshopp00-20&linkCode=df0&hvadid=709870389015&hvpos=&hvnetw=g&hvrand=1232292284651756706&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-1563151464841&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Churrasqueira-Raclette-Ambientes-Antiaderente-Assadeiras/dp/B0DLGM9K71/ref=asc_df_B0DLGM9K71?mcid=35381652bf933739911d836b2e840be4&tag=googleshopp00-20&linkCode=df0&hvadid=709885039576&hvpos=&hvnetw=g&hvrand=8734306404321371980&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2422788073417&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.magazineluiza.com.br/jogo-de-tacas-para-vinho-cristal-450ml-6-pecas-haus-sense-bohemia/p/142270100/ud/tavi/"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Ta%C3%A7as-Sobremesa-Vidro-Ruvolo-Egito/dp/B08F2SF8DG?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&psc=1&smid=A1PPAS91GQH1HT"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Bomboniere-Maracan%C3%A3-Potiche-Cristal-Transparente/dp/B0CWSB4D5N/ref=asc_df_B0CWSB4D5N?mcid=4a6a1cb50e773c48972192a2d1a35cd7&tag=googleshopp00-20&linkCode=df0&hvadid=709968341170&hvpos=&hvnetw=g&hvrand=8908619781943654758&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2292611571774&language=pt_BR&gad_source=4&th=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/JBL-Soundbar-Bluetooth-Cinema-Subwoofer/dp/B0CT922NH7/ref=asc_df_B0CT922NH7?mcid=cc24f636682d3c438382ed4296c54e74&tag=googleshopp00-20&linkCode=df0&hvadid=709964503106&hvpos=&hvnetw=g&hvrand=15003391039270739889&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2287141423132&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Lumin%C3%A1ria-Abajur-Tradicional-Madeira-C%C3%BApula/dp/B0CC9TRHMR/ref=asc_df_B0CC9TRHMR?mcid=ffd034ec24a53406b93dbd8954927eb2&tag=googleshopp00-20&linkCode=df0&hvadid=709968341020&hvpos=&hvnetw=g&hvrand=10632450853683545705&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2200537907248&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://produto.mercadolivre.com.br/MLB-1648573315-2-abajur-toque-cristal-varios-modelos-quarto-mesa-cupula-_JM"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.casasbahia.com.br/aparelho-de-jantar-42-pecas-flamingo-white-oxford/p/1576300866?utm_medium=Cpc&utm_source=google_freelisting&IdSku=1576300866&idLojista=11121&tipoLojista=3P&srsltid=AfmBOorSAIL_7Taiy8UtFCrRGsCVyyimnjUfGjXq1ZWq0IZyDfe7ipO-qV0"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Mesa-cabeceira-Lotus-Off-White/dp/B0B1JLDKLS/ref=asc_df_B0B1JLDKLS?mcid=ffb28e5b29bd31349a29d38a4f16e935&tag=googleshopp00-20&linkCode=df0&hvadid=709968340954&hvpos=&hvnetw=g&hvrand=10150691653280892997&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2014935400690&psc=1&language=pt_BR&gad_source=1"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.mercadolivre.com.br/conjunto-assadeiras-6-pecas-com-tampa-vermelha-marinex/p/MLB27411322?pdp_filters=item_id%3AMLB3463595693&from=gshop&matt_tool=37064035&matt_internal_campaign_id=&matt_word=&matt_source=google&matt_campaign_id=22090354481&matt_ad_group_id=173090607116&matt_match_type=&matt_network=g&matt_device=c&matt_creative=727882733004&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=735125422&matt_product_id=MLB27411322-product&matt_product_partition_id=2389601385546&matt_target_id=aud-1966917494853:pla-2389601385546&cq_src=google_ads&cq_cmp=22090354481&cq_net=g&cq_plt=gp&cq_med=pla&gad_source=1&gad_campaignid=22090354481&gbraid=0AAAAAD93qcBdWSH_8I8m0hdEbl7KRMj5n&gclid=CjwKCAiA8vXIBhAtEiwAf3B-g9g1aZkXGL0qAeBX2hK4pb0zNnhttNIV7XH7Js1YUyrfL2KDguNAFBoC5KYQAvD_BwE"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.mercadolivre.com.br/panela-de-presso-brinox-ceramic-life-pressure-42l-induco/p/MLB26582553?pdp_filters=item_id%3AMLB3427606351&from=gshop&matt_tool=13891073&matt_internal_campaign_id=&matt_word=&matt_source=google&matt_campaign_id=22090193717&matt_ad_group_id=174661961644&matt_match_type=&matt_network=g&matt_device=c&matt_creative=727914179467&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=735098639&matt_product_id=MLB26582553-product&matt_product_partition_id=2388009209946&matt_target_id=aud-1966917494853:pla-2388009209946&cq_src=google_ads&cq_cmp=22090193717&cq_net=g&cq_plt=gp&cq_med=pla&gad_source=1&gad_campaignid=22090193717&gbraid=0AAAAAD93qcCNJFI98d_tLAdPdko0OIUAy&gclid=CjwKCAiA_orJBhBNEiwABkdmjCQL5KFsPUyMZMgauGoiVVnQ2rRarGw0wjsiNVQiSkmLQpbrLW_7oBoCZSAQAvD_BwE"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://www.amazon.com.br/Geladeira-Brastemp-French-Frost-Bro85ak/dp/B0BHX77QJQ/ref=asc_df_B0BHX77QJQ?mcid=d800ce8aaf793175952eedba17afe96c&tag=googleshopp00-20&linkCode=df0&hvadid=709884460975&hvpos=&hvnetw=g&hvrand=494175090328503531&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9100700&hvtargid=pla-2013537609067&psc=1&language=pt_BR&gad_source=4"
+   },
+   {
+      nome: "Cafeteira Elétrica",
+      imagem: "/presentes/cafeteira.jpg",
+      link: "https://loja.electrolux.com.br/micro-ondas-electrolux-preto-23l-efficient-me23p/p?idsku=310119206&gad_source=1&gad_campaignid=22835827172&gbraid=0AAAAADpuC9vLrSM0R6SdQUZs2IsedYkFx&gclid=CjwKCAiA_orJBhBNEiwABkdmjGHJBN1AWBJDQ-QjNVakEyKbSV8kgzNejqsHqgtgj0G_rY-e6tcRoC0xkQAvD_BwE&gclsrc=aw.ds"
+   },
 ];
