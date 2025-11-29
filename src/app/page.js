b1a7e501b7967da71211e5fb3c4f38d47388bb16
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2025-11-29 00:31:49 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import ContadorNovo from "./contador";
+import Navbar from "./NavBar";
 
 
 const poppins = Poppins({
@@ -10,6 +11,7 @@ const poppins = Poppins({
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
+      <Navbar />
       <main className="flex min-h-screen w-full flex-col justify-center items-center 
 bg-[linear-gradient(rgba(0,0,0,0.45),rgba(0,0,0,0.45)),url('/elegant-wedding-couple.png')] 
 bg-cover bg-center">
